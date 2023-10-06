976c595201e1a5478a2ef0ed253417339cbdf4c6
Euro Abo <abaoeuro2002@gmail.com>
2023-10-07 00:03:05 +0800

    cors added

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const port = process.env.PORT;
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
